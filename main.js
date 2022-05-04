9c3e39bbc9eddc0a4f618061b2cf5159fed82650
Harsshil <85441957+Harsshil@users.noreply.github.com>
2022-05-04 14:07:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startClassifictaion(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/M3cajGOM_/model.json' , modelReady);
+}
+   
+function modelReady(){
+    classifier.classify(gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
